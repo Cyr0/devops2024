637578fa2ceb8c63e77206ded777f75134be8865
Ohad Malka <ohadmalka@gmail.com>
2024-02-19 19:50:10 +0200

    major changes on the project

--- terraform/first/main.tf
+++ terraform/first/main.tf
@@ -127,7 +127,6 @@ resource "aws_security_group" "private_efs_security_group" {
 } 
 
 
-
 # subnet-public = 10.12.0.0/17
 resource "aws_subnet" "subnet_public" {
     vpc_id = aws_vpc.project_vpc.id
@@ -175,7 +174,6 @@ resource "aws_subnet" "subnet_private" {
     tags = {
       Name = "${var.project_name}-private-subnet"
     }
-  
 }
 
  # route table creation for private-subnet
@@ -208,14 +206,109 @@ resource "aws_subnet" "subnet_private" {
 # ec2 ami filtering
 # on data.tf
 
+# iam roles from ec2 to storage services
+resource "aws_iam_role" "ec2_storage_role" {
+    name = "ec2_storage_role"
+    assume_role_policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [{
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com",
+        },
+        Effect = "Allow",
+        Sid = "",
+    }],
+  })
+}
+
+# iam policy creation and attachment
+
+
+resource "aws_iam_policy" "ec2_efs_policy" {
+    name = "ec2_efs_policy"
+    description = "Policy for EC2 instances manage EFS file system"
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Action = [
+                    "elasticfilesystem:CreateFileSystem",
+                    "elasticfilesystem:DeleteFileSystem",
+				    "elasticfilesystem:DescribeMountTargets",
+                    "ec2:DescribeAvailabilityZones",
+                    "ec2:CreateNetworkInterface",
+                    "ec2:DeleteNetworkInterface",
+                    "ec2:DescribeInstances",
+                    "ec2-instance-connect:SendSSHPublicKey"
+
+                ],
+                Resource = "*",
+                Effect = "Allow",
+            },
+        ],
+    })
+}
+    
+resource "aws_iam_role_policy_attachment" "ec2_efs_rw_attachment" {
+    role = aws_iam_role.ec2_storage_role.name
+    policy_arn = aws_iam_policy.ec2_efs_policy.arn
+}
+
+# iam role ec2 s3 policy attachment
+resource "aws_iam_role_policy_attachment" "ec2_s3_rw_policy" {
+    role = aws_iam_role.ec2_storage_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# iam instance profile
+resource "aws_iam_instance_profile" "ec2_storage_full_access" {
+    name = "ec2_storage_full_access"
+    role = aws_iam_role.ec2_storage_role.name
+}
 
 # public-ec2 instance - subnet public, security group public
+# public-ec2 instance - user-data
+#sudo mkdir /efs/
+#sudo dnf install -y amazon-efs-utils python3-pip
+#pip3 install botocore
+##sudo mount -t efs ${aws_efs_file_system.efs.id}:/ /efs
+#echo '${aws_efs_file_system.efs.id}:/ /efs efs defaults,_netdev,nofail 0 0' >> /etc/fstab
+#mount -a
+
+# mount efs to public-ec2
+# download ssh key and index.html from s3 to efs share
+# chmod ~/.ssh/test.pem
+# install wget curl
+
+# rsync index.html from public ec2 to private ec2
+ # on private ec2 install 
+  # minikube => jenkins
+
+# from public wget / curl to private ec2 on jenkins port > log
+# rsync log from ec2 private to ec2 public efs share
+# upload log to s3
 resource "aws_instance" "ec2_public" {
-    ami = data.aws_ami.amazon_linux.id
-    instance_type = "${var.ec2_instance_type}"
-    subnet_id = aws_subnet.subnet_public.id
+    ami                    = data.aws_ami.amazon_linux.id
+    instance_type          = "${var.ec2_instance_type}"
+    subnet_id              = aws_subnet.subnet_public.id
     vpc_security_group_ids = [aws_security_group.public_security_group.id]
-    key_name = "${var.private_key_name}"
+    key_name               = "${var.private_key_name}"
+    iam_instance_profile   = aws_iam_instance_profile.ec2_storage_full_access.name
+    user_data              = <<-EOF
+                                #!/bin/bash
+                                dnf update -y
+                                sudo dnf install -y amazon-efs-utils python3-pip
+                                pip3 install botocore ec2instanceconnectcli
+                                sudo pip3 install botocore ec2instanceconnectcli
+                                pwd > /tmp/user_data.log
+                                ls -lha >> /tmp/user_data.log
+                                echo "mssh -r ${var.region} ${aws_instance.ec2_private.id}" > priv.sh
+                                sudo chmod +x ./priv.sh
+                                EOF
+
+    depends_on             = [ aws_iam_role.ec2_storage_role ]
 
     tags = {
       Name = "${var.project_name}-ec2_public"
@@ -225,38 +318,65 @@ resource "aws_instance" "ec2_public" {
 # private ec2 instance - subnet private , security group private
 
 resource "aws_instance" "ec2_private" {
-    ami = data.aws_ami.amazon_linux.id
-    instance_type = "${var.ec2_instance_type}"
-    subnet_id = aws_subnet.subnet_private.id
+    ami                    = data.aws_ami.amazon_linux.id
+    instance_type          = "${var.ec2_instance_type}"
+    subnet_id              = aws_subnet.subnet_private.id
     vpc_security_group_ids = [aws_security_group.private_security_group.id]
-    key_name = "${var.private_key_name}"
-
-    tags = {
-      Name = "${var.project_name}-ec2_private"
+    key_name               = "${var.private_key_name}"
+    iam_instance_profile   = aws_iam_instance_profile.ec2_storage_full_access.name
+    user_data              = <<-EOF
+                                #!/bin/bash
+                                dnf update -y
+                                sudo mkdir /efs/
+                                sudo dnf install -y amazon-efs-utils python3-pip
+                                sudo pip3 install botocore
+                                pip3 install botocore
+                                echo '${aws_efs_file_system.efs.id}:/ /efs efs defaults,_netdev,nofail 0 0' >> /etc/fstab
+                                mount -a
+                                sudo touch /efs/efs.test
+                                ls -lha /efs/ > /tmp/efs.log
+                                EOF
+    depends_on             = [ aws_iam_role.ec2_storage_role ]
+    tags                   = {
+                              Name = "${var.project_name}-ec2_private"
     }
 }
 
 
 # storage
 # create efs for mounting
+
+resource "aws_efs_file_system" "efs" {
+    creation_token = "${var.project_name}-efs"
+    tags = {
+        Name = "${var.project_name}-efs"
+    }  
+}
+resource "aws_efs_mount_target" "efs_mount" {
+    file_system_id = aws_efs_file_system.efs.id
+    subnet_id = aws_subnet.subnet_private.id
+    security_groups = [ aws_security_group.private_efs_security_group.id ]
+    depends_on = [ aws_efs_file_system.efs ]
+}
+
+resource "aws_efs_access_point" "efs_access_point" {
+    file_system_id = aws_efs_file_system.efs.id
+    depends_on = [ aws_efs_file_system.efs ]
+}
+  
 # create s3 and upload key and index.html to s3
 
-# iam roles from ec2 to efs
-# iam roles from ec2 to s3
+resource "random_string" "random_str" {
+    length = 15
+    special = false
+    upper = false
+}
 
+# resource "random_integer" "random_num" {
+#     min = 10000000
+#     max = 99999999
+# }
 
-
-
-# public-ec2 instance - user-data
-# mount efs to public-ec2
-#download ssh key and index.html from s3 to efs share
-#chmod ~/.ssh/test.pem
-#install wget curl
-
-#rsync index.html from public ec2 to private ec2
- # on private ec2 install 
-  # minikube => jenkins
-
-# from public wget / curl to private ec2 on jenkins port > log
-# rsync log from ec2 private to ec2 public efs share
-# upload log to s3
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "${var.project_name}-${random_string.random_str.result}"
+}
