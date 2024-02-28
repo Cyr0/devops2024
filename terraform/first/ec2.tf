c5e879b07a36bf5a47336015d93861081dcd6560
Ohad Malka <ohadmalka@gmail.com>
2024-02-28 12:57:04 +0200

    remove left over dependencies from public_ec2_instance.

--- terraform/first/ec2.tf
+++ terraform/first/ec2.tf
@@ -11,12 +11,12 @@ resource "aws_instance" "ec2_public" {
     #key_name               = "${var.private_key_name}"
     key_name               = aws_key_pair.ssh_key.key_name
 
-    iam_instance_profile   = aws_iam_instance_profile.ec2_storage_full_access.name
-    depends_on             = [ aws_iam_role.ec2_storage_role, aws_instance.ec2_private, 
-                               tls_private_key.rsa, aws_key_pair.ssh_key, 
+    #iam_instance_profile   = aws_iam_instance_profile.ec2_storage_full_access.name
+    #depends_on             = [ aws_iam_role.ec2_storage_role, aws_instance.ec2_private, 
+    #                           tls_private_key.rsa, aws_key_pair.ssh_key, 
+    #                           local_file.ssh_openssh_private_key ] 
+    depends_on             = [ tls_private_key.rsa, aws_key_pair.ssh_key, 
                                local_file.ssh_openssh_private_key ] 
-                               # null_resource.install_putty, null_resource.pem2ppk  ]
-
     tags = {
       Name = "${var.project_name}-ec2_public"
     }
