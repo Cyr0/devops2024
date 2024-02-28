5b381710aaea44b61615e21d60b330a0acae2ba5
Ohad Malka <ohadmalka@gmail.com>
2024-02-28 12:44:32 +0200

    remove not relevant to eks creation objects.

--- terraform/first/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  current_timestamp = timestamp()
-  formatted_timestamp = formatdate("DD-MM-YYYY'T'hh:mm:ss'Z'", local.current_timestamp)
-  custom_formatted_timestamp = replace(local.formatted_timestamp, "T", "-")
-  custom_formatted_timestamp_s = replace(local.formatted_timestamp, ":", "-")
-  final_formatted_timestamp = replace(local.custom_formatted_timestamp, "Z", "")
-}
