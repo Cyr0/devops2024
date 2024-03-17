35c0f408862728212e431eda940890ddae67bcb9
CyRo <cyro@pop-os.localdomain>
2024-03-17 18:53:38 +0200

    vpce call and output changed

--- vpce_project/devops2024/terraform/vpce/vpce.tf
+++ vpce_project/devops2024/terraform/vpce/vpce.tf
@@ -1,19 +1,4 @@
 # Create VPC Endpoint
-# resource "aws_vpc_endpoint" "vpce" {
-#   vpc_id             = aws_vpc.project_vpc.id
-#   service_name       = "com.amazonaws.eu-west-1.s3"
-#   vpc_endpoint_type  = "Interface"
-#   #vpc_endpoint_type  = "Gateway"
-#   security_group_ids = setunion([aws_security_group.public_security_group.id], [aws_security_group.private_security_group.id])
-#   subnet_ids         = setunion([aws_subnet.subnet_public.id])
-
-# }
-
-# data "aws_network_interface" "vpce_eni_ips" {
-#     for_each = toset(aws_vpc_endpoint.vpce.network_interface_ids)
-#     id = each.value
-#     }
-
 module "vpce" {
   source             = "./modules/vpce"  # Adjust the path according to your structure
   vpc_id             = aws_vpc.project_vpc.id
@@ -27,7 +12,9 @@ module "vpce" {
 }
 
 data "aws_network_interface" "vpce_eni_ips" {
-    for_each = toset(module.vpce.network_interface_ids)
-    id = each.value
+   for_each = toset(module.vpce.network_interface_ids)
+   id = each.value
+   
+   depends_on = [module.vpce]
 }
 
